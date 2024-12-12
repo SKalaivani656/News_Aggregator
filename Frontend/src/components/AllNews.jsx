aec0d6e7505ab4ff122aa8f3dcc4cce32f6a55cd
KALAIVANI SAKTHI <164200816+SKalaivani656@users.noreply.github.com>
2024-12-12 21:06:54 +0530

    Update AllNews.jsx

--- Frontend/src/components/AllNews.jsx
+++ Frontend/src/components/AllNews.jsx
@@ -23,7 +23,7 @@ function AllNews() {
   useEffect(() => {
     setIsLoading(true);
     setError(null);
-    fetch(`https://news-aggregator-tqp5.onrender.com/all-news?page=${page}&pageSize=${pageSize}`)
+    fetch(`http://news-aggregator-tqp5.onrender.com/all-news?page=${page}&pageSize=${pageSize}`)
       .then(response => {
         if (response.ok) {
           return response.json();
