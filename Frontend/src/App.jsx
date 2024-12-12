d8ebf4f457a1f9367c7d1994a9b530f6549c71c9
KALAIVANI SAKTHI <164200816+SKalaivani656@users.noreply.github.com>
2024-12-12 20:00:10 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import React from "react";
 import Header from "./components/Header";
 import AllNews from "./components/AllNews";
 import Footer from "./components/Footer";
 import TopHeadlines from "./components/TopHeadlines";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CountryNews from "./components/CountryNews";
 
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [],
-  resolve: {
-    extensions: ['.js', '.jsx'], // Ensure .jsx files are resolved correctly
-  },
-});
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <div className="w-full">
       
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }} >
         <Header />
         <Routes>
-          <Route path="." element={<AllNews />} />
+          <Route path="/" element={<AllNews />} />
           <Route path="/top-headlines/:category" element={<TopHeadlines />} />
           <Route path="/country/:iso" element={<CountryNews />} />
         </Routes>
